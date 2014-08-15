c9f89cffff2feab71ebf1f0af63624313a3c6dd4
Philipp Weissensteiner <mail@philippweissensteiner.com>
2014-08-15 16:09:42 +0200

    Add runner and runs to seeds.
    This basically adds the stuff from the README to seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+runner = Runner.create name: "Bolt 1", hardware: "xeon xyz 32GB - 2.2GHZ", token: 123
+
+Run.create(
+  "runner_id"       => runner.id,
+  "ruby_version"    => "2.0.0p353",
+  "git_hash"        => "609cdd3236dff10405453fe2e201ce615ce203b4",
+  "rails_version"   => "4.0.0",
+  "rails_git_hash"  => "7c4bfe",
+  "date"            =>  "2013-12-18T12 => 09 => 16+00 => 00",
+  "results_attributes" => [
+    {"benchmark" => "some_bench", "score" => "222"}
+  ]
+)
+
+Run.create({
+  "runner_id"       => runner.id,
+  "ruby_version"    => "2.1.0-p247",
+  "git_hash"        => "c0a04be8ddcadcd89c02bc135e4b288f6d936714",
+  "rails_version"   => "4.0.0",
+  "rails_git_hash"  => "7c4bfe",
+  "date"            =>  "2014-01-18T12 => 09 => 16+00 => 00",
+  "results_attributes" => [
+    {"benchmark" => "some_bench", "score" => "4222"}
+  ]
+})
